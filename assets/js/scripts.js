0c47d27a4730bab03a9398c26c69d87ef42554a3
Ramesh Velishala <ramesh.velishala@trigyn.com>
2020-11-18 14:24:15 +0530

    Worked on to add data update section in the footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,4 +14,27 @@
         e.preventDefault();
         $("body").toggleClass("sb-sidenav-toggled");
     });
+	let $updatedOn_ = document.querySelector('#updatedOn');
+	fetch('https://cbpfapi.unocha.org/vo2/odata/LastModified')
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                //$div1_.textContent = JSON.stringify(data);
+                var obj = data;
+                if ($updatedOn_ != undefined && $updatedOn_ != null)
+                    $updatedOn_.textContent = ConvertJsonDateTime(obj.value[0].last_updated_date);
+            });
+        } else console.log('Network response was not ok.');
+    })
+    .catch(function(error) {
+        console.log('Fetch error: ');
+    });
+
+function ConvertJsonDateTime(jsonDate) {
+    var date = new Date(jsonDate);
+    var month = date.getMonth() + 1;
+    month = month.toString().length > 1 ? month : "0" + month;
+
+    return date.getDate() + "/" + month + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
 })(jQuery);
