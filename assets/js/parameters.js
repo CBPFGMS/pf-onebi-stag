5f44b6a1d4f08a4da1134eb247788d8d54937740
Gerardo Furtado <gerardofurtado@gmail.com>
2021-08-26 22:15:57 +1000

    Update parameters.js

--- assets/js/parameters.js
+++ assets/js/parameters.js
@@ -1,9 +1,9 @@
 //|Initial parameters
 
 const parameters = {
-	chart: "allocationsByCountry",
-	fund: "total",
-	showClosedFunds: true,
+	chart: "allocationsByCountry", //chart displayed when the page loads
+	fund: "total", //type of fund: total, cerf/cbpf, cerf, cbpf
+	showClosedFunds: true, //show or hide closed funds
 	cerfFirstYear: null, //if 'null' the first year will be the first year in the data
 	cbpfFirstYear: null //if 'null' the first year will be the first year in the data
 };
