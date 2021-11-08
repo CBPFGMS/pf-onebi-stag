fe12f0031937d02b94b855bc450988c5cfbf86b1
Gerardo Furtado <gerardofurtado@gmail.com>
2021-11-08 13:51:06 +1100

    Update parameters.js

--- assets/js/parameters.js
+++ assets/js/parameters.js
@@ -4,7 +4,8 @@ const parameters = {
 	fund: "total", //type of fund: total, cerf/cbpf, cerf, cbpf
 	showClosedFunds: true, //show or hide closed funds
 	cerfFirstYear: null, //if 'null' the first year will be the first year in the data
-	cbpfFirstYear: null //if 'null' the first year will be the first year in the data
+	cbpfFirstYear: null, //if 'null' the first year will be the first year in the data
+	countryProfile: null //selected country in the country profile section. Use the numeric country ID.
 };
 
 export { parameters };
