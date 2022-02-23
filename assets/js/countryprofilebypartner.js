0507418c0645ab704a8f55ecb412b081160aaa88
Gerardo Furtado <gerardofurtado@gmail.com>
2022-02-23 14:31:37 +1000

    Create countryprofilebypartner.js

---
+++ assets/js/countryprofilebypartner.js
@@ -0,0 +1,141 @@
+import { chartState } from "./chartstate.js";
+
+//|constants
+const classPrefix = "pfbicpbypartner",
+	formatPercent = d3.format(".0%"),
+	formatSIaxes = d3.format("~s"),
+	currentDate = new Date(),
+	localVariable = d3.local(),
+	currentYear = currentDate.getFullYear(),
+	separator = "##",
+	buttonsList = ["total", "cerf/cbpf", "cerf", "cbpf"],
+	allocationTypes = {
+		cbpf: ["1", "2"],
+		cerf: ["3", "4"]
+	}; //THIS SHOULD NOT BE HARDCODED
+
+
+
+function createCountryProfileByPartner(container, lists, colors, tooltipDiv) {
+
+	const outerDiv = container.append("div")
+		.attr("class", classPrefix + "outerDiv");
+
+	const cerfId = +Object.keys(lists.fundTypesList).find(e => lists.fundTypesList[e] === "cerf");
+	const cbpfId = +Object.keys(lists.fundTypesList).find(e => lists.fundTypesList[e] === "cbpf");
+
+	function draw(originalData) {
+
+		//end of draw
+	};
+
+	return draw;
+
+	//end of createCountryProfileOverview
+};
+
+function createFundButtons(container, colors) {
+	const buttons = container.selectAll(null)
+		.data(buttonsList)
+		.enter()
+		.append("button")
+		.classed("active", d => chartState.selectedFund === d);
+
+	const bullet = buttons.append("span")
+		.attr("class", "icon-circle")
+		.append("i")
+		.attr("class", (_, i) => i === 1 ? "fas fa-adjust fa-xs" : "fas fa-circle fa-xs")
+		.style("color", (d, i) => i !== 1 ? colors[d] : null);
+
+	const title = buttons.append("span")
+		.html(d => " " + (d === "total" ? capitalize(d) : d.toUpperCase()));
+};
+
+
+function mouseOut(tooltip) {
+	tooltip.html(null)
+		.style("display", "none");
+};
+
+function positionTooltip(tooltip, container, event, position) {
+	let top, left;
+
+	const containerSize = container.node().getBoundingClientRect(),
+		tooltipSize = tooltip.node().getBoundingClientRect(),
+		elementSize = event.currentTarget.getBoundingClientRect();
+
+	if (position === "right") {
+		top = elementSize.top - containerSize.top + (elementSize.height / 2) - (tooltipSize.height / 2) + "px";
+		left = elementSize.right + tooltipHorizontalPadding + tooltipSize.width - containerSize.left > containerSize.width ?
+			elementSize.left - tooltipSize.width - containerSize.left - tooltipHorizontalPadding + "px" :
+			elementSize.right - containerSize.left + tooltipHorizontalPadding + "px";
+	} else if (position === "top") {
+		top = Math.max(0, elementSize.top - containerSize.top - tooltipSize.height - tooltipVerticalPadding) + "px";
+		left = Math.max(0, Math.min(containerSize.width - tooltipSize.width - tooltipHorizontalPadding,
+			elementSize.left - containerSize.left + (elementSize.width / 2) - (tooltipSize.width / 2))) + "px";
+	} else if (position === "left") {
+		top = elementSize.top - containerSize.top + (elementSize.height / 2) - (tooltipSize.height / 2) + "px";
+		left = Math.max(0, elementSize.left - tooltipSize.width - containerSize.left - tooltipHorizontalPadding) + "px";
+	};
+
+	tooltip.style("top", top)
+		.style("left", left);
+};
+
+function applyColors(selection, colors) {
+	selection.style("color", chartState.selectedFund === "total" || chartState.selectedFund === "cerf/cbpf" ?
+		colors.total : d3.color(colors[chartState.selectedFund]).darker(darkerValue));
+};
+
+function capitalize(str) {
+	return str[0].toUpperCase() + str.substring(1)
+};
+
+function formatSIFloat(value) {
+	const length = (~~Math.log10(value) + 1) % 3;
+	const digits = length === 1 ? 2 : length === 2 ? 1 : 0;
+	return d3.formatPrefix("." + digits, value)(value);
+};
+
+function formatSIFloatNoZeroes(value) {
+	const length = (~~Math.log10(value) + 1) % 3;
+	const digits = length === 1 ? 2 : length === 2 ? 1 : 0;
+	return d3.formatPrefix("." + digits + "~", value)(value);
+};
+
+function reverseFormat(s) {
+	if (+s === 0) return 0;
+	let returnValue;
+	const transformation = {
+		Y: Math.pow(10, 24),
+		Z: Math.pow(10, 21),
+		E: Math.pow(10, 18),
+		P: Math.pow(10, 15),
+		T: Math.pow(10, 12),
+		G: Math.pow(10, 9),
+		B: Math.pow(10, 9),
+		M: Math.pow(10, 6),
+		k: Math.pow(10, 3),
+		h: Math.pow(10, 2),
+		da: Math.pow(10, 1),
+		d: Math.pow(10, -1),
+		c: Math.pow(10, -2),
+		m: Math.pow(10, -3),
+		μ: Math.pow(10, -6),
+		n: Math.pow(10, -9),
+		p: Math.pow(10, -12),
+		f: Math.pow(10, -15),
+		a: Math.pow(10, -18),
+		z: Math.pow(10, -21),
+		y: Math.pow(10, -24)
+	};
+	Object.keys(transformation).some(k => {
+		if (s.indexOf(k) > 0) {
+			returnValue = parseFloat(s.split(k)[0]) * transformation[k];
+			return true;
+		}
+	});
+	return returnValue;
+};
+
+export { createCountryProfileByPartner };
