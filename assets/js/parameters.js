5e6202633498b6bcc3b069605ed987a22863a05d
Gerardo Furtado <gerardofurtado@gmail.com>
2022-12-30 01:17:07 +1000

    No closed funds

--- assets/js/parameters.js
+++ assets/js/parameters.js
@@ -4,7 +4,7 @@ const parameters = {
 	//The accepted values are: "allocationsByCountry", "allocationsBySector", "allocationsByType", "allocationsByMonth", "contributionsByCerfCbpf" and "contributionsByDonor"
 	fund: "total", //type of fund: total, cerf/cbpf, cerf, cbpf
 	year: null, //if 'null' the year will be the current year
-	showClosedFunds: true, //show or hide closed funds
+	showClosedFunds: false, //show or hide closed funds
 	cerfFirstYear: null, //if 'null' the first year will be the first year in the data
 	cbpfFirstYear: null, //if 'null' the first year will be the first year in the data
 	countryProfile: null //selected country in the country profile section. Use the numeric country ID.
